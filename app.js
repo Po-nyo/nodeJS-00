e5ea59dfdf6b05eaf88c5e3332c5854a60b6fadd
WooKyungjin <wkj1103@naver.com>
2020-08-11 18:23:13 +0900

    url refactor

--- app.js
+++ app.js
@@ -1,20 +1,7 @@
 let express = require('express');
 let bodyParser = require('body-parser');
-let mysql = require('mysql');
 let main = require('./router/main');
-let form = require('./router/form');
-let email_post = require('./router/email_post');
-
-// database connection
-let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'test',
-    password: 'test',
-    database: 'test_db'
-});
-
-connection.connect();
-
+let email = require('./router/email');
 
 let app = express();
 
@@ -29,27 +16,5 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/', main);
-app.use('/form', form);
-app.use('/email_post', email_post);
+app.use('/email', email);
 
-app.post('/ajax_send_email', function(req, res) {
-    const email = req.body.email;
-    console.log(email);
-
-    let responseData = {}
-
-    connection.query('SELECT name FROM user WHERE email="' + email +'"', function(err, rows) {
-        if(err) throw err;
-        if(rows[0]) {
-            console.log(rows[0]);
-            responseData.result = 'ok';
-            responseData.name = rows[0].name;
-        }
-        else {
-            responseData.result = 'none';
-            responseData.name = '';
-            console.log('none: ' + rows[0]);
-        }
-        res.json(responseData)
-    })
-})
---
+++ router/email.js
@@ -0,0 +1,49 @@
+let express = require('express');
+let router = express.Router();
+let path = require('path')
+let mysql = require('mysql');
+
+// DATABASE SETTINGS
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'test',
+    password: 'test',
+    database: 'test_db'
+});
+
+connection.connect();
+
+// ROUTER
+router.get('', function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/form.html"));
+});
+
+router.post('/email_post', function(req, res) {
+    res.render('email.ejs', {
+        'email': req.body.email,
+    })
+});
+
+router.post('/ajax', function(req, res) {
+    const email = req.body.email;
+    console.log(email);
+
+    let responseData = {}
+
+    connection.query('SELECT name FROM user WHERE email="' + email +'"', function(err, rows) {
+        if(err) throw err;
+        if(rows[0]) {
+            console.log(rows[0]);
+            responseData.result = 'ok';
+            responseData.name = rows[0].name;
+        }
+        else {
+            responseData.result = 'none';
+            responseData.name = '';
+            console.log('none: ' + rows[0]);
+        }
+        res.json(responseData)
+    })
+})
+
+module.exports = router;
